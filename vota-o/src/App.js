9e8c8b3dd3c69de5d8396dee5a6e4483bbcdf3d1
isavieira121245 <visadora178@gmail.com>
2023-11-27 14:40:15 +0000

    vota-o

---
+++ vota-o/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Votacao from './votacao'; 
+ 
+function App() {
+  return (
+    <div className="App">
+      <h1>Simulador de Enquete</h1>
+      <Votacao /> 
+    </div>
+  );
+}
+ 
+export default App;
