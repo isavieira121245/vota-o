c75d8d5cfb7bde5d6c3b79654a675bc20fa73129
isavieira121245 <visadora178@gmail.com>
2023-11-27 14:57:32 +0000

    vota-o

--- vota-o/src/App.js
+++ vota-o/src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './App.css';
-import Votacao from './votacao.css'; 
+import Votacao from './componentes/votacao'; 
  
 function App() {
   return (
     <div className="App">
       <h1>Simulador de Enquete</h1>
-      <Votacao /> 
+      <Votacao />
     </div>
   );
 }
--- vota-o/src/componentes/votacao.js
+++ vota-o/src/componentes/votacao.js
@@ -6,10 +6,10 @@ const Votacao = () => {
     opcao1: 0,
     opcao2: 0,
     opcao3: 0,
-    // Adicione mais opções conforme necessário
+   
   });
  
-  const handleVoto = (opcao) => {
+  const Voto = (opcao) => {
     setVotos({
       ...votos,
       [opcao]: votos[opcao] + 1,
@@ -20,9 +20,9 @@ const Votacao = () => {
     <div className="votacao">
       <h2>Qual seu sabor de sorvete favorito?</h2>
       <div className="opcoes">
-        <button onClick={() => handleVoto('opcao1')}>Opção 1</button>
-        <button onClick={() => handleVoto('opcao2')}>Opção 2</button>
-        <button onClick={() => handleVoto('opcao3')}>Opção 3</button>
+        <button onClick={() => Voto('opcao1')}>Opção 1</button>
+        <button onClick={() => Voto('opcao2')}>Opção 2</button>
+        <button onClick={() => Voto('opcao3')}>Opção 3</button>
         {/* Adicione mais botões ou opções de voto conforme necessário */}
       </div>
       <div className="resultados">
