8dba53d3803127223c061c4ef1991c5fa7cb93ba
isavieira121245 <visadora178@gmail.com>
2023-11-27 14:51:17 +0000

    vota-o

--- vota-o/src/votacao.js
+++ vota-o/src/votacao.js
@@ -6,9 +6,10 @@ const Votacao = () => {
     opcao1: 0,
     opcao2: 0,
     opcao3: 0,
+    // Adicione mais opções conforme necessário
   });
  
-  const Voto = (opcao) => {
+  const handleVoto = (opcao) => {
     setVotos({
       ...votos,
       [opcao]: votos[opcao] + 1,
@@ -17,18 +18,19 @@ const Votacao = () => {
  
   return (
     <div className="votacao">
-      <h2>Qual seu time favorito?</h2>
+      <h2>Qual seu sabor de sorvete favorito?</h2>
       <div className="opcoes">
-        <button onClick={() => Voto('opcao1')}>Opção 1</button>
-        <button onClick={() => Voto('opcao2')}>Opção 2</button>
-        <button onClick={() => Voto('opcao3')}>Opção 3</button>
-        
+        <button onClick={() => handleVoto('opcao1')}>Opção 1</button>
+        <button onClick={() => handleVoto('opcao2')}>Opção 2</button>
+        <button onClick={() => handleVoto('opcao3')}>Opção 3</button>
+        {/* Adicione mais botões ou opções de voto conforme necessário */}
       </div>
       <div className="resultados">
         <h3>Resultados:</h3>
-        <p>São Paulo: {votos.opcao1}</p>
-        <p>Corinthians: {votos.opcao2}</p>
-        <p>Santos: {votos.opcao3}</p>
+        <p>Opção 1: {votos.opcao1}</p>
+        <p>Opção 2: {votos.opcao2}</p>
+        <p>Opção 3: {votos.opcao3}</p>
+        {/* Exiba mais resultados conforme necessário */}
       </div>
     </div>
   );
